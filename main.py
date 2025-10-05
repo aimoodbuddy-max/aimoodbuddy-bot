715309c8543a311e14b60f16f182e9ae94fc98dc
aimoodbuddy-max <aimoodbuddy@gmail.com>
2025-10-05 22:13:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -56,4 +56,7 @@ def handle_message(event: MessageEvent):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    import os
+    port = int(os.environ.get("PORT", 8000))  # dùng PORT do Render cấp, nếu không có thì fallback 8000
+    uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
+
